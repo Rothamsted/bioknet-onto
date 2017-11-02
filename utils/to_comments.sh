72d4063fa04a9249d711381f80df2b2ebd24daae
Marco Brandizi <marco.brandizi@gmail.com>
2017-11-03 00:13:17 +0100

    making LODE-compatible copy.

---
+++ utils/to_comments.sh
@@ -0,0 +1,6 @@
+export JENA_HOME='/Applications/local/dev/semantic_web/jena'
+export PATH="/Users/brandizi/bin:$PATH"
+
+tmpf=$(mktemp /tmp/tocomm_XXX.nt)
+"$JENA_HOME/bin/update" --data=../bioknet.owl --update=descr2comments.sparul --dump >$tmpf
+"$JENA_HOME/bin/update" --data=$tmpf --update=note2comments.sparul --dump >lode_bioknet.owl
