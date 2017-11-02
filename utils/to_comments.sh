8405a5da31e3ccc6e3b6787069fe70d1e9ed9fba
Marco Brandizi <marco.brandizi@gmail.com>
2017-11-03 00:16:05 +0100

    Adding LODE-compatible copy

--- utils/to_comments.sh
+++ utils/to_comments.sh
@@ -3,4 +3,4 @@ export PATH="/Users/brandizi/bin:$PATH"
 
 tmpf=$(mktemp /tmp/tocomm_XXX.nt)
 "$JENA_HOME/bin/update" --data=../bioknet.owl --update=descr2comments.sparul --dump >$tmpf
-"$JENA_HOME/bin/update" --data=$tmpf --update=note2comments.sparul --dump >lode_bioknet.owl
+"$JENA_HOME/bin/update" --data=$tmpf --update=note2comments.sparul --dump >lode_bioknet.nt
