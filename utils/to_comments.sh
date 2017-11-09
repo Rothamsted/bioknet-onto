cfdf76452c6b1c1f79ec766569270f93aa99ce7a
Marco Brandizi <marco.brandizi@gmail.com>
2017-11-09 21:48:18 +0000

    Fixing issues with LODE.

--- utils/to_comments.sh
+++ utils/to_comments.sh
@@ -3,4 +3,6 @@ export PATH="/Users/brandizi/bin:$PATH"
 
 tmpf=$(mktemp /tmp/tocomm_XXX.nt)
 "$JENA_HOME/bin/update" --data=../bioknet.owl --update=descr2comments.sparul --dump >$tmpf
-"$JENA_HOME/bin/update" --data=$tmpf --update=note2comments.sparul --dump >lode_bioknet.nt
+"$JENA_HOME/bin/update" --data=$tmpf --update=note2comments.sparul --dump | sponge $tmpf
+"$JENA_HOME/bin/update" --data=$tmpf --update=aggregate_comments.sparul --dump | sponge $tmpf
+"$JENA_HOME/bin/update" --data=$tmpf --update=delete_comments.sparul --dump >lode_bioknet.nt
