451eedf63ae8ebf12d3921f338549d6847db46df
Marco Brandizi <marco.brandizi@gmail.com>
2018-04-06 19:12:13 +0100

    Adding CSV Import Example.

--- examples/bmp_reg_human/bk_convert.sh
+++ examples/bmp_reg_human/bk_convert.sh
@@ -3,23 +3,36 @@
 # Stores the results in bkout, as Turtle files, and also puts all the data (original and converted) into
 # a temp TDB.
 #
-# Requires JENA_HOME to be defined
+# Requires JENA_HOME, TARQL_HOME to be defined
 #
 
 start_dir=$(pwd)
 cd $(dirname "$0")
 my_dir=$(pwd)
 
-tdb="/tmp/bk_convert_tdb"
-
-echo "Loading data into temp TDB $tdb"
-rm -Rf "$tdb"
-"$JENA_HOME/bin/tdbloader2" --loc="$tdb" ../../bioknet.owl ../../bk_ondex.owl ./*.{rdf,owl,ttl}
-
-echo "Running CONSTRUCTs"
 out_dir="./bkout"
 mkdir -p "$out_dir"
 rm -f "$out_dir/*"
+
+
+echo "Converting CSVs from DEG Example"
+tarql_cmd="$TARQL_HOME/bin/tarql --tabs --quotechar '\"'"
+for i in $(seq 2)
+do
+  "$TARQL_HOME/bin/tarql" --tabs --quotechar '"' \
+    cvt_sample_degs_$i.tarql input_data/sample_degs_$i.tsv \
+    >"$out_dir/sample_degs_$i.ttl"
+done
+
+mv input_data/bkonto/*.ttl "$out_dir"
+
+echo "Loading data into temp TDB $tdb"
+tdb="/tmp/bk_convert_tdb"
+rm -Rf "$tdb"
+"$JENA_HOME/bin/tdbloader2" --loc="$tdb" ../../bioknet.owl ../../bk_ondex.owl input_data/*.{rdf,owl,ttl}
+
+
+echo "Running CONSTRUCTs"
 
 sparql="$JENA_HOME/bin/tdbquery"
 for query in ./cvt_*.sparql
@@ -29,7 +42,8 @@ do
   "$sparql" --loc "$tdb" --query "$query" >"$out_dir/${qbase}.ttl"
 done
 
-echo "Putting output files together and loading into temp TDB"
+
+echo "Putting output files together"
 cd "$out_dir"
 cat *.ttl >all.ttl
 
@@ -37,12 +51,14 @@ cd "$my_dir/../.."
 onto_dir="$(pwd)"
 cd "$my_dir"
 
-echo "Adding our own ontologies"
+
+echo "Loading all, together with our own ontologies"
 "$JENA_HOME/bin/tdbloader" --loc="$tdb" \
   "$out_dir/all.ttl" \
   "$onto_dir/bioknet.owl" \
   "$onto_dir/bk_ondex.owl" \
   "$onto_dir/bk_mappings.ttl"
+
 
 echo "Adding external ontologies"
 
---
+++ examples/bmp_reg_human/input_data/bkonto/readme.txt
@@ -0,0 +1,3 @@
+
+Data in this folder are already in the BK-Onto format, so they don't need any conversion and 
+they will be moved to bkout/ as-is.
